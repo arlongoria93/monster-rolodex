422a8a58ce95e9dddbfc4126fa47588771776c36
AngeloRLongoria <arlongoria93@gmail.com>
2020-09-08 21:53:10 -0500

    s

---
+++ build/precache-manifest.e182e0d707677c3680e2d8000d4e820c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "851e363b61dbbfcafaa71bcc0fb31e7c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "fd637690a35e142587e4",
+    "url": "/monster-rolodex/static/css/main.5ef44c82.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd637690a35e142587e4",
+    "url": "/monster-rolodex/static/js/main.dce27d84.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
